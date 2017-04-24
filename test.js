e2f770ad0e1da6357d01d202f59d2c483d549c15
Jaret Burkett <jaretburkett@gmail.com>
2017-04-23 20:16:55 -0500

    Auto detect binary. Simplified config with hostname append.

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 var os = require('os');
 console.log('Arch:',os.arch());
 console.log('CPUs:', os.cpus().length);
+console.log('hostname:', os.hostname().split('.')[0]);
 console.log('Platform:', os.platform()); // darwin, linux, win32
